e1d8bc47dc91c65d4caee8e65f5af3c1b72f9d40
Jon <jonnmb@gmail.com>
2016-02-08 17:35:04 -0700

    Forgot to pluralize the relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User
   include Mongoid::Document
   
-  has_and_belongs_to_many :group
+  has_and_belongs_to_many :groups
   
   field :userName, type: String
   field :passHash, type: String
