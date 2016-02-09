14def48703d321c991a26bafa9937e317fe4255f
ninjapretzel <ninjapretzel@yahoo.com>
2016-02-09 16:26:01 -0700

    ignores Gemfile/Gemfile.lock

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,9 @@ class User
   
   field :register_date, type: DateTime, :default => DateTime.now
   
-  validates_presence_of :passHash
+  validates_presence_of :pass_hash
   validates_uniqueness_of :email
-  validates_uniqueness_of :pubKey
+  validates_uniqueness_of :pub_key
   
   
 end
