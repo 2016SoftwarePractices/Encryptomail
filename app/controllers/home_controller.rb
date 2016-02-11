5391fd20a6364ccd54eeed1a6079a8668d1f7e08
ninjapretzel <ninjapretzel@yahoo.com>
2016-02-11 11:51:51 -0700

    Adds Stuff into home_controller.rb, changes register page to use form_for to generate form tags

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,23 @@
 class HomeController < ApplicationController
   def index
   end
+  
+  def login
+  end
+  
+  def register
+	@user = User.new
+	@user.email = 'enter email here'
+	@user.pub_key = 'Paste ASCII-Armored PGP key here'
+	@user.pass_hash = 'Enter Password'
+	
+  end
+  
+  
+  
+  
+  def registration_params 
+	params.require(:user).permit(:email, :pub_key, :pass_hash)
+  end
+  
 end
