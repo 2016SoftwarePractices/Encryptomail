d4e5356ea3dd74ea2eb235ca4a746a1dcd175f20
Jon <jonnmb@Gmail.com>
2016-02-16 12:05:32 -0700

    Group creation now working properly

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ Bundler.require(*Rails.groups)
 module Encryptomail
   class Application < Rails::Application
     
-    Domain = "@encryptomail.org"
+    $Domain = "@encryptomail.org"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
