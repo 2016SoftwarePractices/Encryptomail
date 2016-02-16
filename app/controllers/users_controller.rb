2ad826193f577445db793e33e94c86b29b251878
Jon <jonnmb@Gmail.com>
2016-02-16 02:35:28 -0700

    user controller setup wrong

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -73,6 +73,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.permit(:pass_hash, :password1, :password2, :email, :pub_key)
+			params.require(:user).permit(:pass_hash, :password1, :password2, :email, :pub_key)
     end
 end
