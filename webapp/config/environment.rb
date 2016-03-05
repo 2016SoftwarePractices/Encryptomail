927cfb7c80c599b059f6e8af4c8481413a545c63
ninjapretzel <ninjapretzel@yahoo.com>
2016-03-05 16:40:24 -0700

    Moves password to file ignored by .gitignore file - get this from the trello.

--- webapp/config/environment.rb
+++ webapp/config/environment.rb
@@ -5,13 +5,16 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 
-#config.action_mailer.
+passwordFile = File.new("password.txt")
+pword = passwordFile.gets
+passwordFile.close
+
 ActionMailer::Base.smtp_settings = {
 	:address              => "smtp.gmail.com",
 	:domain               => "mail.gmail.com",
 	:port                 => 587,
 	:user_name            => 'encryptomail@gmail.com',
-	:password             => 'WALMARTskypeROPELAPTOPlaptophuluTOKYO',
+	:password             => pword,
 	:authentication       => :login,
 	:enable_starttls_auto => true
 }
