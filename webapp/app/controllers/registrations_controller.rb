4d705529fbe88d04d2c84b589f00183ee0d3d323
jon <jonnmb@gmail.com>
2016-03-08 15:50:06 -0700

    Initial recaptcha work

--- webapp/app/controllers/registrations_controller.rb
+++ webapp/app/controllers/registrations_controller.rb
@@ -4,7 +4,15 @@ class RegistrationsController < Devise::RegistrationsController
 	end
 
 	def create
+		if verify_recaptcha(model: @user)
+			puts "nice"
+		else
+			puts "You messed up"
+			redirect_to new_user_registration_path, alert: "You failed the captcha"
+			return
+		end
 		super
+
 		UserMailer.welcome_email(resource).deliver_now
 		
 		#insert pubkey into user
