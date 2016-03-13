c722843b7d7504c6598f6308ebc7d56daf4e64de
Jon <jonnmb@Gmail.com>
2016-03-12 21:04:33 -0700

    Fixed it maybe?

--- emailapp/decrypter.rb
+++ emailapp/decrypter.rb
@@ -1,37 +1,38 @@
+require 'rubygems'
+require 'openpgp'
+require 'open-uri'
 
 module EmailApp
 
-require 'rubygems'
-require 'openpgp'
-
-class EmailApp::Decrypter
+	class EmailApp::Decrypter
     
-    #This method will receive an email and a passphrase from the user registration view
-    def self.generatePGPkey (name, email, passphrase)
+   		#This method will receive an email and a passphrase from the user registration view
+    		def self.generatePGPkey (name, email, passphrase)
         
-        gpg = OpenPGP::Engine::GnuPG.new(:homedir => '~/.gnupg')
+        		gpg = OpenPGP::Engine::GnuPG.new(:homedir => '~/.gnupg')
         
-        key_id = gpg.gen_key({
-        :key_type      => 'RSA',
-        :key_length    => 4096,
-        :subkey_type   => 'ELG-E',
-        :subkey_length => 1024,
-        :name          => name,
-        :comment       => nil,
-        :email         => email,
-        :passphrase    => passphrase,
-        })
-        return key_id
-    end
+        		key_id = gpg.gen_key({
+        		:key_type      => 'RSA',
+        		:key_length    => 4096,
+        		:subkey_type   => 'ELG-E',
+        		:subkey_length => 1024,
+        		:name          => name,
+        		:comment       => nil,
+        		:email         => email,
+        		:passphrase    => passphrase,
+        		})
+        		return key_id
+    		end
     
-    def self.commandLinePGPkeygen (name, email, passphrase)
+    		def self.commandLinePGPkeygen (name, email, passphrase)
         
-    end
+    		end
     
-    def decryptPGP (toBeDecrypted)
-        require 'open-uri'
-        msg = OpenPGP::Message.parse(OpenPGP.dearmor(toBeDecrypted))
-        return msg
-    end
+    		def decryptPGP (toBeDecrypted)
+        		#require 'open-uri'
+        		msg = OpenPGP::Message.parse(OpenPGP.dearmor(toBeDecrypted))
+        		return msg
+    		end
     
+	end
 end
--- emailapp/encryptomail.rb
+++ emailapp/encryptomail.rb
@@ -1,43 +1,43 @@
-
-module EmailApp
-
 require 'rubygems'
 require 'gpgme'
 
-class EmailApp::Encryptomail
+module EmailApp
+
+	class EmailApp::Encryptomail
     
-# generate_key(parms, pubkey = Data.new, seckey = Data.new)
-# Generate a new key pair. parms is a string which looks like
+	# generate_key(parms, pubkey = Data.new, seckey = Data.new)
+	# Generate a new key pair. parms is a string which looks like
 
-#  <GnupgKeyParms format="internal">
-#  Key-Type: DSA
-#  Key-Length: 1024
-#  Subkey-Type: ELG-E
-#  Subkey-Length: 1024
-#  Name-Real: Joe Tester
-#  Name-Comment: with stupid passphrase
-#  Name-Email: joe@foo.bar
-#  Expire-Date: 0
-#  Passphrase: abc
-#  </GnupgKeyParms>
-# If pubkey and seckey are both set to nil, it stores the generated key pair into your key ring.
+	#  <GnupgKeyParms format="internal">
+	#	  Key-Type: DSA
+	#  Key-Length: 1024
+	#  Subkey-Type: ELG-E
+	#  Subkey-Length: 1024
+	#  Name-Real: Joe Tester
+	#  Name-Comment: with stupid passphrase
+	#  Name-Email: joe@foo.bar
+	#  Expire-Date: 0
+	#  Passphrase: abc
+	#  </GnupgKeyParms>
+	# If pubkey and seckey are both set to nil, it stores the generated key pair into your key ring.
 
-    def self.generatePGPkey()
-        ctx = GPGME::Ctx.new()
+    		def self.generatePGPkey()
+        		ctx = GPGME::Ctx.new()
         
-        fields = '<GnupgKeyParms format="internal">
-                    Key-Type: DSA
-                    Key-Length: 1024
-                    Subkey-Type: ELG-E
-                    Subkey-Length: 1024
-                    Name-Real: Ross Baldwin
-                    Name-Comment: with stupid passphrase
-                    Name-Email: joe@foo.bar
-                    Expire-Date: 0
-                    Passphrase: abc
-                    </GnupgKeyParms>'
+       			fields = '<GnupgKeyParms format="internal">
+                    		Key-Type: DSA
+                    		Key-Length: 1024
+                    		Subkey-Type: ELG-E
+                    		Subkey-Length: 1024
+                    		Name-Real: Ross Baldwin
+                    		Name-Comment: with stupid passphrase
+                    		Name-Email: joe@foo.bar
+                    		Expire-Date: 0
+                    		Passphrase: abc
+                    		</GnupgKeyParms>'
         
-        ctx.generate_key(fields, nil, nil)
-        # Key IS NOT RETURNED, it is added to the Linux local keychain
-    end
+        		ctx.generate_key(fields, nil, nil)
+        		# Key IS NOT RETURNED, it is added to the Linux local keychain
+    		end
+	end
 end
