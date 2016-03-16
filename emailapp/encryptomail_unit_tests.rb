a9a1b5774d7c69b969abf2adc6bce5b2b7d78a87
P. Ross Baldwin <ross@rossbaldwin.com>
2016-03-16 05:28:04 +0000

    Ross: Added some methods to EmailApp::Encryptomail - private key export! woo! Also modified the key generation method to properly accept custom params - wrote tests for new methods

--- emailapp/encryptomail_unit_tests.rb
+++ emailapp/encryptomail_unit_tests.rb
@@ -6,14 +6,16 @@ module EmailApp
 
 	class EncryptomailUnitTests
 
-    		def self.testGPGmekeygen()
+          #test PGP keypair generation
+    		def self.testGPGmekeygen(name, email, passphrase)
       		puts "TEST: Attempting to test the creation of a key using GPGme"
       		puts "(GPGme) Trying to generate PGP key - this may take a while: "
-      		key_string = EmailApp::Encryptomail.generatePGPkeyGPGme('GPGmetestuser', 'gpgmetestuser@test.com', 'H5giu)(*')
+      		key_string = EmailApp::Encryptomail.generatePGPkeyGPGme(name, email, passphrase)
       		puts key_string
       		puts "GPGme has successfully created a new keypair for GPGmetestuser"
     		end
     		
+    		  #test public key export
     		def self.testGPGmeexportpublickey(email)
     		  puts "TEST: Attempting to export public key for #{email}"
     		  # functions = Encryptomail.new
@@ -21,20 +23,31 @@ module EmailApp
      			EmailApp::Encryptomail.exportpublickeyGPGme(email)
     		end
     		
-    		def self.testGPGmelistkeys()
-    			puts "Attempting to list all keys in the Linux keychain"
-     			pri_key = GPGME::Key.find :public
-     			  # pub_key IS AN ARRAY OF GPGME::DATA OBJECTS
-     			pri_key.each do |key|
-     				data = key.export(:armor => true)
-     				data.to_s
-     			end
+    		
+          #test private key export
+    		def self.testprivatekeyexport(email)
+    		    puts "TEST: Attempting to export private key for gpgmetestuser@test.com"
+    		    EmailApp::Encryptomail.exportprivatekey(email)
     		end
+    		
+    		
+    		  #test key listing
+    		def self.testGPGmelistkeys()
+    			EmailApp::Encryptomail.listallpublickeys()
+    			EmailApp::Encryptomail.listallprivatekeys()
+    		end
+    		
+    		
+    		def self.testkeydeletion(email)
+    		  EmailApp::Encryptomail.deleteuserkeys(email)
+    		end
+
 	end
 
 # Main()
-# EncryptomailUnitTests.testGPGmekeygen
-puts EmailApp::EncryptomailUnitTests.testGPGmeexportpublickey("gpgmetestuser@test.com")
+puts EmailApp::EncryptomailUnitTests.testGPGmekeygen("Tester1", "tester1@test.com", "00010101")
+puts EmailApp::EncryptomailUnitTests.testGPGmeexportpublickey("tester1@test.com")
+puts EmailApp::EncryptomailUnitTests.testprivatekeyexport("tester1@test.com")
 puts EmailApp::EncryptomailUnitTests.testGPGmelistkeys()
-
+#puts EmailApp::EncryptomailUnitTests.testkeydeletion("gpgmetestuser@test.com")
 end
