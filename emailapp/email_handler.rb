94aacb5fc318655aaf675dfebfaa4b8d23291301
P. Ross Baldwin <ross@rossbaldwin.com>
2016-04-09 20:27:04 +0000

    Deleted the smtp_message.rb file -> unneeded : moved to deprecated code

--- emailapp/email_handler.rb
+++ emailapp/email_handler.rb
@@ -1,6 +1,7 @@
-#Email handling code
-#code is commented out until it gets converted from pseudocode to Ruby
-
+=begin
+    Email handling code
+    This is the core business logic of the Encryptomail application
+=end
 
 module EmailApp
     
@@ -171,46 +172,3 @@ module EmailApp
     end
     
 end
-
-
-
-
-
-
-
-=begin
-
-#input = $stdin.read
-#puts input
-
-module EmailApp
-
-    class Email_handler
-    
-        #Pseudocode method for email handling
-        def self.email_handler ( rawEmail )
-            #The rawEmail parameter is the entire email, formatted as a string.
-            #Includes headers, subject, body, etc.
-            Parse WHO the recipient is
-            groupEmailAddress = Parse group email recipient from text
-            plaintextEmail = decrypt(rawEmail)
-            #the decrypt method ALSO filters out everything not in ASCII armor - yay!
-            sendLoop(plaintextEmail, groupEmailAddress)
-            
-        end
-        
-        
-        def self.sendLoop(plaintextEmail, groupEmailAddress)
-            groupMemberList = Find members of groupEmailAddress
-            foreach member in groupMemberList
-                #encrypt email for the individual member
-                encryptedEmail = encrypt(plaintextEmail, member)
-                #send email to the group member
-                send(encryptedEmail, member)
-            end
-        end
-        
-    end
-
-end
-=end
