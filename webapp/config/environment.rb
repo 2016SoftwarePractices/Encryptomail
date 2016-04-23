b4a8a8e0a3ede4543b630c065e2e0ed1a83f3f56
ninjapretzel <ninjapretzel@yahoo.com>
2016-04-23 11:49:07 -0600

    Removes loading of password file, now that mailer is setup to use localhost, this is no longer needed.

--- webapp/config/environment.rb
+++ webapp/config/environment.rb
@@ -4,11 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-
-passwordFile = File.new("password.txt")
-pword = passwordFile.gets
-passwordFile.close
-
 ActionMailer::Base.smtp_settings = {
 	:address              => 'localhost',
 	:domain               => 'encryptomail.xyz',
