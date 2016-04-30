5515ec3e20a98c40119d4212bd6adf9a1b878407
Jacob Fisher <zombiefish90@gmail.com>
2016-04-30 14:06:13 -0600

    Got the check in for making sure email and pgp key match on user creation.

--- webapp/lib/keygenerator.rb
+++ webapp/lib/keygenerator.rb
@@ -14,19 +14,24 @@ class KeyGenerator
 		ctx.generate_key(fields, nil, nil)
 	end
 
-	def self.exportpublickeyGPGme(email)
-		pub_key = GPGME::Key.find(:public, "#{email}")
-		data = pub_key.first.export(:armor => true)
-	end
-	
 	def self.importkey(pubkey)
 		GPGME::Key.import(pubkey)
 	end
+	
+	def self.exportpublickeyGPGme(email)
+		pub_key = keyFinder(email)
+		data = pub_key.export(:armor => true)
+	end
 
 	def self.deletekey(email)
-		pub_key = GPGME::Key.find(:public, "#{email}")
+		pub_key = keyFinder(email)
 		ctx = GPGME::Ctx.new()
-		ctx.delete_key(pub_key[0], allow_secret = true)
+		ctx.delete_key(pub_key, allow_secret = true)
+	end
+	
+	def self.keyFinder(email)
+		pub_key = GPGME::Key.find(:public, "#{email}")
+		pub_key[0]
 	end
 
 	def self.buildparamsblock(name, email, passphrase)
